812b1162d24585786be167134af8f43af2c1c7d4
Nick0603 <b10303008@gmail.com>
2017-07-23 05:15:41 +0800

    ui page2 setting 頁面 初步排版完成

---
+++ app/src/main/java/com/goodmoring/SettingActivity.java
@@ -0,0 +1,16 @@
+package com.goodmoring;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * Created by Nick on 2017/7/23.
+ */
+
+public class SettingActivity extends Activity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_setting);
+    }
+}
