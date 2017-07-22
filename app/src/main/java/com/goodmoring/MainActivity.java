f47ed1f11289ab22f5c280c227461779418c873b
Nick0603 <b10303008@gmail.com>
2017-07-23 04:05:14 +0800

    ui alarmTotal page v1.1 將checkbox 可以切換圖片的功能加上，但目前無儲存紀錄功能

--- app/src/main/java/com/goodmoring/MainActivity.java
+++ app/src/main/java/com/goodmoring/MainActivity.java
@@ -8,8 +8,8 @@ import android.view.View;
 import android.widget.ImageButton;
 
 public class MainActivity extends AppCompatActivity {
-
-    ImageButton iBtnAdd;
+    public Boolean booleanCheckBox[] = {true,false,true};
+    ImageButton iBtnAdd,iBtnCheckAlarm1,iBtnCheckAlarm2,iBtnCheckAlarm3;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -19,6 +19,10 @@ public class MainActivity extends AppCompatActivity {
 
     void initView(){
         iBtnAdd =  (ImageButton)findViewById(R.id.iBtn_add);
+        iBtnCheckAlarm1 =  (ImageButton)findViewById(R.id.iBtn_checkAlarm1);
+        iBtnCheckAlarm2 =  (ImageButton)findViewById(R.id.iBtn_checkAlarm2);
+        iBtnCheckAlarm3 =  (ImageButton)findViewById(R.id.iBtn_checkAlarm3);
+
         iBtnAdd.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -26,6 +30,42 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
+        iBtnCheckAlarm1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if((booleanCheckBox[0])){
+                    iBtnCheckAlarm1.setImageResource(R.mipmap.checkbox_off);
+                    booleanCheckBox[0] = false;
+                }else{
+                    iBtnCheckAlarm1.setImageResource(R.mipmap.checkbox_on);
+                    booleanCheckBox[0] = true;
+                }
+            }
+        });
+        iBtnCheckAlarm2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if((booleanCheckBox[1])){
+                    iBtnCheckAlarm2.setImageResource(R.mipmap.checkbox_off);
+                    booleanCheckBox[1] = false;
+                }else{
+                    iBtnCheckAlarm2.setImageResource(R.mipmap.checkbox_on);
+                    booleanCheckBox[1] = true;
+                }
+            }
+        });
+        iBtnCheckAlarm3.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if((booleanCheckBox[2])){
+                    iBtnCheckAlarm3.setImageResource(R.mipmap.checkbox_off);
+                    booleanCheckBox[2] = false;
+                }else{
+                    iBtnCheckAlarm3.setImageResource(R.mipmap.checkbox_on);
+                    booleanCheckBox[2] = true;
+                }
+            }
+        });
     }
 
     private void showDialog() {
